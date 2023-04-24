e5207bf491826bd69ad60c083d916fe59a57e3d8
Eugene Kim <eugene02876@gmail.com>
2023-04-24 22:59:17 +0900

    [feat]: 디테일 페이지 완성!

--- pages/[name].tsx
+++ pages/[name].tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import styles from '../styles/detail.module.scss';
 import DetailHeader from "@/components/home/DetailHeader";
 import DetailContent from "@/components/home/DetailContent";
+import useCurrentStore from "@/hooks/useCurrentStore";
 
 interface Props {
     store : Store;
@@ -12,12 +13,20 @@ interface Props {
 const StoreDetail: NextPage<Props> = ({store}) => {
     const expanded = true;
     const router = useRouter();
+    const { setCurrentStore } = useCurrentStore();
+  
+    const goToMap = () => {
+      setCurrentStore(store);
+      router.push(`
+        /?zoom=15&lat=${store.coordinates[0]}&lng=${store.coordinates[1]}
+      `);
+    };
     return (
         <div className = {`${styles.detailSection} ${styles.expanded} ${styles.selected}}`}>
            <DetailHeader
             currentStore={store}
             expanded={expanded}
-            onClickArrow={() => null}
+            onClickArrow={goToMap}
             />
             <DetailContent currentStore = {store} expanded = {expanded}/>
         </div>
